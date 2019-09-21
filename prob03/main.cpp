c9424673fc6dc2427ffd32b9050652137b385315
JimmyC1204 <jimmyc1204@cs104u-3d.acad.fullerton.edu>
2019-09-21 14:05:51 -0700

    Created a program that randomizes a rectable's length

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,25 @@
 // Random Rectangle
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+
+int main()
+{
+  int max = 40;
+  int num;
+  unsigned seed = time(0);
+  srand(seed);
+  num = rand() % max + 1;
+
+
+  std::string dub;
+  dub.assign(num,'#');
+
+  std::cout << dub << std::endl;
+  std::cout << dub << std::endl;
+  std::cout << dub << std::endl;
+
+
+  return 0;
+}
